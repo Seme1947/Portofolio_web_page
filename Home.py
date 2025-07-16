56b15d046527104dcf826d2001cc2b8567d76498
Seme <semeniuc_mihai2001@yahoo.com>
2025-07-16 10:05:38 +0300

    Finish the app, adding real links to our apps

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import csv
 
 st.set_page_config(layout = 'wide')
 
@@ -15,3 +16,31 @@ with col2:
     Thank you.
     """
     st.info(content)
+
+content2 = """
+    Here you will see my projects. Thank you for watching.
+    """
+st.write(content2)
+
+col3,col_empty,col4 = st.columns([1.5,0.5,1.5])
+
+with col3:
+    with open("data.csv", 'r') as file:
+        data = list(csv.reader(file))
+
+    apps = []
+
+    for dat in data[1:]:
+        apps.append(dat[0].split(';'))
+
+    for i in range(0,len(apps),2):
+        st.write(apps[i][0])
+        st.info(apps[i][1])
+        st.image(f'images/images/{i + 1}.png')
+        st.write(f"[Source Code]({apps[i][2]})")
+with col4:
+    for i in range(1, len(apps),2):
+        st.write(apps[i][0])
+        st.info(apps[i][1])
+        st.image(f'images/images/{i + 1}.png')
+        st.write("[Source Code](https://pythonhow.com)")
